4be43e81700afc413a40a0c95780beb88d7fd5ba
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-07-30 20:15:45 +0530

    provider given

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import CounterSlice from './slices/CounterSlice'
 
 export const store = configureStore({
-  reducer: CounterSlice,
+    //the name of the reducer is the name given to it in the slice where the slice was created
+    counter: CounterSlice,
 })
-
+ 
