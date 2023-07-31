c806d9884490c76853f78eefb5bb1ce1e354ab4d
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-07-31 19:04:37 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Cart from "./pages/Cart"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
     <div>
-      App
+      <div>
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="*" element={<div>Page not found</div>}/>
+      </Routes>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,30 @@
+import {FaShoppingCart} from "react-icons/fa"
+import { NavLink } from "react-router-dom";
+
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+  <div>
+    <div className="flex flex-row justify-between">
+      <NavLink to={}>
+        <div>
+          <img src="#"/>
+        </div>
+      </NavLink>
+        <div>
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          <NavLink to="/cart">
+            <div>
+              <FaShoppingCart/>
+            </div>
+          </NavLink>
+        </div>
+    </div>
+  </div>
+  )
+  
 };
 
 export default Navbar;
